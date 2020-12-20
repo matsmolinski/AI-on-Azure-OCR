05beb5de7ddbe07263c3d4b8f80a91e93ab98173
KonradMagiera <k.magiera1@wp.pl>
2020-12-20 17:00:01 +0100

    remove binding; create only one Client for all files

--- ocr_proj/Upload_image_to_blob/__init__.py
+++ ocr_proj/Upload_image_to_blob/__init__.py
@@ -6,7 +6,7 @@ import azure.functions as func
 from azure.storage.blob import BlobServiceClient, BlobClient
 
 
-def main(req: func.HttpRequest, outputblob: func.Out[func.InputStream]) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Received request to post image')
 
     files_len = len(req.files.to_dict().items())
@@ -19,13 +19,15 @@ def main(req: func.HttpRequest, outputblob: func.Out[func.InputStream]) -> func.
             f'Missing file in form-data body',
             status_code=406
             )
+    
+    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
     for input_file in req.files.values():
         extension = input_file.filename.split('.')[1]
+        file_uuid = str(uuid.uuid4())
         logging.info('Adding file to blob storage...')
 
-        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-        blob_client = blob_service_client.get_blob_client(container=container,blob=str(uuid.uuid4()) + "." + extension)
+        blob_client = blob_service_client.get_blob_client(container=container,blob=file_uuid + "." + extension)
         blob_client.upload_blob(input_file)
         logging.info('File added')
 
