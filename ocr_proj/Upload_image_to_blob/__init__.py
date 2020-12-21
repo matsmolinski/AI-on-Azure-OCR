bf30bfd09a07772cbaf957ff6a54b2e971e235e0
KatarzynaWolska <kasia.wolska137@gmail.com>
2020-12-21 15:26:56 +0100

    Added error handling

--- ocr_proj/Upload_image_to_blob/__init__.py
+++ ocr_proj/Upload_image_to_blob/__init__.py
@@ -20,28 +20,29 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             f'Missing file in form-data body',
             status_code=406
             )
-            
-    table_service = TableService(connection_string=connect_str)
-    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
-    for input_file in req.files.values():
-        extension = input_file.filename.split('.')[1]
-        file_uuid = str(uuid.uuid4())
-        filename = file_uuid + "." + extension
-        logging.info('Adding file to blob storage...')
+    try:        
+        table_service = TableService(connection_string=connect_str)
+        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
-        blob_client = blob_service_client.get_blob_client(container=container,blob=file_uuid + "." + extension)
-        blob_client.upload_blob(input_file)
-        task = {'PartitionKey': extension, 'RowKey': filename, 'email': '', 'data_analysis': ''}
-        table_service.insert_entity('Tasks', task)
-        logging.info('File added')
+        for input_file in req.files.values():
+            extension = input_file.filename.split('.')[1]
+            file_uuid = str(uuid.uuid4())
+            filename = file_uuid + "." + extension
+            logging.info('Adding file to blob storage...')
 
-    # example read form table_service
-    # tasks = table_service.query_entities('Tasks', filter="PartitionKey eq 'png'")
-    # for task in tasks:
-    #     logging.warn(task.data_analysis)
+            blob_client = blob_service_client.get_blob_client(container=container,blob=file_uuid + "." + extension)
+            blob_client.upload_blob(input_file)
+            task = {'PartitionKey': extension, 'RowKey': filename, 'email': '', 'data_analysis': ''}
+            table_service.insert_entity('Tasks', task)
+            logging.info('File added')
 
-    return func.HttpResponse(
-        f'File added for processing',
-        status_code=200
-        )
+        return func.HttpResponse(
+            f'File added for processing',
+            status_code=200
+            )
+    except Exception as e:
+        return func.HttpResponse(
+            f'Error has occured',
+            status_code=400
+            )
