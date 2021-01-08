d32a07aa1a53d053b0b0eb213ad078a462b9a85f
pachniar <01130999@pw.edu.pl>
2021-01-08 16:58:49 +0100

    Funkcja przekazująca plik z blob do cognitive services.

---
+++ ocr_proj/Send_image_to_CognitiveServices/__init__.py
@@ -0,0 +1,64 @@
+import logging
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from msrest.authentication import CognitiveServicesCredentials
+import azure.functions as func
+from datetime import datetime, timedelta
+from azure.storage.blob import ResourceTypes, AccountSasPermissions, generate_account_sas
+
+
+def main(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob \n"
+                 f"Name: {myblob.name}\n"
+                 f"Blob Size: {myblob.length} bytes\n"
+                 f"Path to: {myblob.uri}\n")
+    # subscription_key = os.environ["CognitiveServicesVisionKey"]
+    # endpoint = os.environ["CognitiveServicesVisionEndpoint"]
+    subscription_key = "a2194b2fe92541e3aa0a4299a1d74fa5"
+    endpoint = "https://pogodpolvision.cognitiveservices.azure.com/"
+
+    # GENERACJA DOSTĘPU
+    #----------------------------------------------------------------------------------------------------
+
+        sas_token = generate_account_sas(
+            blob_service_client.account_name,
+            account_key=blob_service_client.credential.account_key,
+            resource_types=ResourceTypes(object=True),
+            permission=AccountSasPermissions(read=True),
+            expiry=datetime.utcnow() + timedelta(hours=1)
+        )
+
+    # ZAPYTANIE
+    #----------------------------------------------------------------------------------------------------
+
+    computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
+
+    # Call API with URL and raw response (allows you to get the operation location)
+    recognize_handw_results = computervision_client.read(myblob)
+
+    # POBRANIE ODPOWIEDZI
+    #----------------------------------------------------------------------------------------------------
+
+    # Get the operation location (URL with an ID at the end) from the response
+    operation_location_remote = recognize_handw_results.headers["Operation-Location"]
+    # Grab the ID from the URL
+    operation_id = operation_location_remote.split("/")[-1]
+
+    # Call the "GET" API and wait for it to retrieve the results 
+    while True:
+        get_handw_text_results = computervision_client.get_read_result(operation_id)
+        if get_handw_text_results.status not in ['notStarted', 'running']:
+            break
+        time.sleep(1)
+
+    # Print the detected text, line by line
+    if get_handw_text_results.status == OperationStatusCodes.succeeded:
+        for text_result in get_handw_text_results.analyze_result.read_results:
+            for line in text_result.lines:
+                print(line.text)
+                print(line.bounding_box)
+    print()
+
+
+
