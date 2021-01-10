a1e635a8efe511b7d15bc74a44cf4184876a22d0
matsmolinski <matsmolinski@gmail.com>
2021-01-10 16:59:32 +0100

    cognitive requests & table storage communication

--- ocr_proj/Send_image_to_CognitiveServices/__init__.py
+++ ocr_proj/Send_image_to_CognitiveServices/__init__.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import time
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
@@ -6,6 +8,8 @@ from msrest.authentication import CognitiveServicesCredentials
 import azure.functions as func
 from datetime import datetime, timedelta
 from azure.storage.blob import ResourceTypes, AccountSasPermissions, generate_account_sas
+from azure.cosmosdb.table.tableservice import TableService
+from azure.cosmosdb.table.models import Entity
 
 
 def main(myblob: func.InputStream):
@@ -13,22 +17,62 @@ def main(myblob: func.InputStream):
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes\n"
                  f"Path to: {myblob.uri}\n")
-    # subscription_key = os.environ["CognitiveServicesVisionKey"]
-    # endpoint = os.environ["CognitiveServicesVisionEndpoint"]
-    subscription_key = "rypka"
-    endpoint = "https://pogodpolvision.cognitiveservices.azure.com/"
+    vision_key = os.environ["CognitiveServicesVisionKey"]
+    vision_endpoint = os.environ["CognitiveServicesVisionEndpoint"]
+    connect_str = os.environ["AzureWebJobsStorage"]
+    container = os.environ["ContainerName"]
+    rowkey = myblob.name.split('.')[0].split('/')[1]
+    partkey = myblob.name.split('.')[1]
+    #try:        
+    table_service = TableService(connection_string=connect_str)
+    logging.info(f"row: {rowkey}\n"
+                f"part: {partkey}\n")
+    task = table_service.get_entity('Tasks', partkey, rowkey)
+    del task['etag']
+    logging.info(task)
+
+
+    #cognitive stuff here
+    computervision_client = ComputerVisionClient(vision_endpoint, CognitiveServicesCredentials(vision_key))
+    read_results = computervision_client.read(myblob.uri,  raw=True)
+
+    operation_location_remote = read_results.headers["Operation-Location"]
+    operation_id = operation_location_remote.split("/")[-1]
+
+    while True:
+        get_read_results = computervision_client.get_read_result(operation_id)
+        if get_read_results.status not in ['notStarted', 'running']:
+            break
+        time.sleep(1)
+    text = ""
+    if get_read_results.status == OperationStatusCodes.succeeded:
+        for text_result in get_read_results.analyze_result.read_results:
+            for line in text_result.lines:
+                text = text + line.text + '\n'
+    #endof cognitive stuff
+
+
+    task['translation'] = 'abc'
+    task['image_text'] = text
+    task['data_analysis'] = 'ghi'
+    table_service.update_entity('Tasks', task)
+    logging.info('Table storage updated')
+
+    #except Exception:
+    #    msg = 'Error has occured'
+     #   logging.warn(msg)
 
     # GENERACJA DOSTĘPU
     #----------------------------------------------------------------------------------------------------
-
-        sas_token = generate_account_sas(
-            blob_service_client.account_name,
-            account_key=blob_service_client.credential.account_key,
-            resource_types=ResourceTypes(object=True),
-            permission=AccountSasPermissions(read=True),
-            expiry=datetime.utcnow() + timedelta(hours=1)
-        )
-
+    '''
+    sas_token = generate_account_sas(
+        blob_service_client.account_name,
+        account_key=blob_service_client.credential.account_key,
+        resource_types=ResourceTypes(object=True),
+        permission=AccountSasPermissions(read=True),
+        expiry=datetime.utcnow() + timedelta(hours=1)
+    )
+    
     # ZAPYTANIE
     #----------------------------------------------------------------------------------------------------
 
@@ -59,6 +103,6 @@ def main(myblob: func.InputStream):
                 print(line.text)
                 print(line.bounding_box)
     print()
-
+    '''
 
 
