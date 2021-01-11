066a7bd2af47e100b2b33c00ce1f07f58d5b24d5
pachniar <01130999@pw.edu.pl>
2021-01-11 17:22:42 +0100

    Dodanie analizy sentymentu do tekstu odczytanego z pliku

--- ocr_proj/Send_image_to_CognitiveServices/__init__.py
+++ ocr_proj/Send_image_to_CognitiveServices/__init__.py
@@ -5,6 +5,8 @@ import requests
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
 from msrest.authentication import CognitiveServicesCredentials
 import azure.functions as func
 from datetime import datetime, timedelta
@@ -20,7 +22,10 @@ def main(myblob: func.InputStream):
                  f"Path to: {myblob.uri}\n")
     vision_key = os.environ["CognitiveServicesVisionKey"]
     vision_endpoint = os.environ["CognitiveServicesVisionEndpoint"]
+    sentiment_key = os.environ["CognitiveServicesSentimentKey"]
+    sentiment_enpoint = os.environ["CognitiveServicesSentimentEndpoint"]
     connect_str = os.environ["AzureWebJobsStorage"]
+
     rowkey = myblob.name.split('.')[0].split('/')[1]
     partkey = myblob.name.split('.')[1]
 
@@ -33,7 +38,7 @@ def main(myblob: func.InputStream):
     logging.info(task)
     language = task.language
 
-    #cognitive stuff here
+    #vision stuff here
     computervision_client = ComputerVisionClient(vision_endpoint, CognitiveServicesCredentials(vision_key))
     read_results = computervision_client.read(myblob.uri,  raw=True)
 
@@ -51,12 +56,34 @@ def main(myblob: func.InputStream):
             print(get_read_results.analyze_result.read_results)
             for line in text_result.lines:
                 text = text + line.text + '\n'
-    #endof cognitive stuff
+    #endof vision stuff\
 
+
+    #sentiment stuff here
+    textdoc = [text]
+    text_sentiment = ""
+    sen_credential = AzureKeyCredential(sentiment_key)
+    text_analytics_client = TextAnalyticsClient(endpoint=sentiment_enpoint, credential=sen_credential)
+    response = text_analytics_client.analyze_sentiment(documents = textdoc)[0]
+    text_sentiment = text_sentiment + "Document Sentiment: {}".format(response.sentiment)
+    text_sentiment = text_sentiment +"Overall scores: positive={0:.2f}; neutral={1:.2f}; negative={2:.2f} \n".format(
+        response.confidence_scores.positive,
+        response.confidence_scores.neutral,
+        response.confidence_scores.negative,
+    )
+    for idx, sentence in enumerate(response.sentences):
+        text_sentiment = text_sentiment +"Sentence: {}".format(sentence.text)
+        text_sentiment = text_sentiment +"Sentence {} sentiment: {}".format(idx+1, sentence.sentiment)
+        text_sentiment = text_sentiment +"Sentence score:\nPositive={0:.2f}\nNeutral={1:.2f}\nNegative={2:.2f}\n".format(
+                sentence.confidence_scores.positive,
+                sentence.confidence_scores.neutral,
+                sentence.confidence_scores.negative,
+            )
+    #endof sentiment stuff
 
     task['translation'] = 'abc'
     task['image_text'] = text
-    task['data_analysis'] = 'ghi'
+    task['data_analysis'] = text_sentiment
     table_service.update_entity('Tasks', task)
     logging.info('Table storage updated')
 
