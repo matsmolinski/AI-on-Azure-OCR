101a8a6f2509700ef27ee7521c8600b9d58f8cb1
matsmolinski <matsmolinski@gmail.com>
2021-01-11 15:27:30 +0100

    Adding sendgrid to cognitive service app

--- ocr_proj/Send_image_to_CognitiveServices/__init__.py
+++ ocr_proj/Send_image_to_CognitiveServices/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import time
+import requests
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
@@ -20,9 +21,9 @@ def main(myblob: func.InputStream):
     vision_key = os.environ["CognitiveServicesVisionKey"]
     vision_endpoint = os.environ["CognitiveServicesVisionEndpoint"]
     connect_str = os.environ["AzureWebJobsStorage"]
-    container = os.environ["ContainerName"]
     rowkey = myblob.name.split('.')[0].split('/')[1]
     partkey = myblob.name.split('.')[1]
+
     #try:        
     table_service = TableService(connection_string=connect_str)
     logging.info(f"row: {rowkey}\n"
@@ -30,7 +31,7 @@ def main(myblob: func.InputStream):
     task = table_service.get_entity('Tasks', partkey, rowkey)
     del task['etag']
     logging.info(task)
-
+    language = task.language
 
     #cognitive stuff here
     computervision_client = ComputerVisionClient(vision_endpoint, CognitiveServicesCredentials(vision_key))
@@ -47,6 +48,7 @@ def main(myblob: func.InputStream):
     text = ""
     if get_read_results.status == OperationStatusCodes.succeeded:
         for text_result in get_read_results.analyze_result.read_results:
+            print(get_read_results.analyze_result.read_results)
             for line in text_result.lines:
                 text = text + line.text + '\n'
     #endof cognitive stuff
@@ -58,51 +60,16 @@ def main(myblob: func.InputStream):
     table_service.update_entity('Tasks', task)
     logging.info('Table storage updated')
 
+    url = "https://prod-145.westeurope.logic.azure.com:443/workflows/ebdbfc4977234440b9a48a98a9fa36af/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1wlap-WyQ-Qi9WYsFRliBWAdXCSPg9aljr7LupDnHsI"
+    content = {
+        'email': task.email,
+        'code': rowkey
+    }
+    x = requests.post(url, json=content)
     #except Exception:
     #    msg = 'Error has occured'
      #   logging.warn(msg)
 
-    # GENERACJA DOSTĘPU
-    #----------------------------------------------------------------------------------------------------
-    '''
-    sas_token = generate_account_sas(
-        blob_service_client.account_name,
-        account_key=blob_service_client.credential.account_key,
-        resource_types=ResourceTypes(object=True),
-        permission=AccountSasPermissions(read=True),
-        expiry=datetime.utcnow() + timedelta(hours=1)
-    )
-    
-    # ZAPYTANIE
-    #----------------------------------------------------------------------------------------------------
-
-    computervision_client = ComputerVisionClient(endpoint, CognitiveServicesCredentials(subscription_key))
-
-    # Call API with URL and raw response (allows you to get the operation location)
-    recognize_handw_results = computervision_client.read(myblob)
-
-    # POBRANIE ODPOWIEDZI
-    #----------------------------------------------------------------------------------------------------
-
-    # Get the operation location (URL with an ID at the end) from the response
-    operation_location_remote = recognize_handw_results.headers["Operation-Location"]
-    # Grab the ID from the URL
-    operation_id = operation_location_remote.split("/")[-1]
-
-    # Call the "GET" API and wait for it to retrieve the results 
-    while True:
-        get_handw_text_results = computervision_client.get_read_result(operation_id)
-        if get_handw_text_results.status not in ['notStarted', 'running']:
-            break
-        time.sleep(1)
-
-    # Print the detected text, line by line
-    if get_handw_text_results.status == OperationStatusCodes.succeeded:
-        for text_result in get_handw_text_results.analyze_result.read_results:
-            for line in text_result.lines:
-                print(line.text)
-                print(line.bounding_box)
-    print()
-    '''
+   
 
 
