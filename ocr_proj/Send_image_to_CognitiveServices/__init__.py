e82be3a4485e43b921f7f9dad317cf86c87a675c
pachniar <01130999@pw.edu.pl>
2021-01-13 16:26:05 +0100

    Dodanie tłumaczenia tekstu

--- ocr_proj/Send_image_to_CognitiveServices/__init__.py
+++ ocr_proj/Send_image_to_CognitiveServices/__init__.py
@@ -1,7 +1,4 @@
-import logging
-import os
-import time
-import requests
+import os, requests, uuid, json, time, logging
 from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 from azure.cognitiveservices.vision.computervision.models import VisualFeatureTypes
@@ -20,10 +17,21 @@ def main(myblob: func.InputStream):
                  f"Name: {myblob.name}\n"
                  f"Blob Size: {myblob.length} bytes\n"
                  f"Path to: {myblob.uri}\n")
-    vision_key = os.environ["CognitiveServicesVisionKey"]
-    vision_endpoint = os.environ["CognitiveServicesVisionEndpoint"]
-    sentiment_key = os.environ["CognitiveServicesSentimentKey"]
-    sentiment_enpoint = os.environ["CognitiveServicesSentimentEndpoint"]
+    # vision_key = os.environ["CognitiveServicesVisionKey"]
+    # vision_endpoint = os.environ["CognitiveServicesVisionEndpoint"]
+    # sentiment_key = os.environ["CognitiveServicesSentimentKey"]
+    # sentiment_enpoint = os.environ["CognitiveServicesSentimentEndpoint"]
+    # translation_key = os.environ["CognitiveServicesTranslationKey"]
+    # translation_endpoint = os.environ["CognitiveServicesTranslationEndpoint"]
+    # translation_loc = "westeurope"
+    # connect_str = os.environ["AzureWebJobsStorage"]
+    vision_key = "fc0bd2284030410cad911605f2b8bc16"
+    vision_endpoint = "https://pogodpol-analycs.cognitiveservices.azure.com/"
+    sentiment_key = "f02788a53b2347c389ebf7861d1cad9b"
+    sentiment_enpoint = "https://pogodpol-analycs.cognitiveservices.azure.com/"
+    translation_key = "9a5e58d5e7e946f88a82ccc973fb8591"
+    translation_endpoint = "https://api.cognitive.microsofttranslator.com/"
+    translation_loc = "westeurope"
     connect_str = os.environ["AzureWebJobsStorage"]
     
 
@@ -57,8 +65,8 @@ def main(myblob: func.InputStream):
             print(get_read_results.analyze_result.read_results)
             for line in text_result.lines:
                 text = text + line.text + '\n'
-    #endof vision stuff\
-
+    text = text.replace("\n", " ")
+    #endof vision stuff
 
     #sentiment stuff here
     textdoc = [text]
@@ -82,13 +90,55 @@ def main(myblob: func.InputStream):
             )
     #endof sentiment stuff
 
-    task['translation'] = 'abc'
+
+    #translation stuff
+    if language is None or len(language)<2:
+        translation = ''
+    else:
+        path = '/translate?api-version=3.0'
+        params = '&to={}'.format(language)
+        constructed_url = translation_endpoint + path + params
+        headers = {
+            'Ocp-Apim-Subscription-Key': translation_key,
+            'Ocp-Apim-Subscription-Region': translation_loc,
+            'Content-type': 'application/json',
+            'X-ClientTraceId': str(uuid.uuid4())
+        }
+        body = [{
+            'text' : text
+        }]
+        request = requests.post(constructed_url, headers=headers, json=body)
+        response = request.json()
+        print(json.dumps(response, sort_keys=True, indent=4, separators=(',', ': ')))
+        response = json.dumps(response)
+        response = response.split("text")
+        response = response[1]
+        size = len(response)
+        response = response[4:size-5]
+        response = response.split(", \"to\":")
+        size = len(response)
+        translate_lang=response[size-1]
+        response.pop()
+        translate_text = ""
+        for elem in response:
+            translate_text = translate_text + elem
+        translate_lang = translate_lang.replace("\"","")
+        translate_lang = translate_lang.strip()
+        translation = "Language: {}, Text: {}".format(translate_lang, translate_text)
+
+
+    #endof translation
+
+
+    task['translation'] = translation
     task['image_text'] = text
     task['data_analysis'] = text_sentiment
     table_service.update_entity('Tasks', task)
     logging.info('Table storage updated')
+    print(task)
 
-    url = os.environ["EmailURL"]
+    # url = os.environ["EmailURL"]
+    url = "https://prod-145.westeurope.logic.azure.com:443/workflows/ebdbfc4977234440b9a48a98a9fa36af/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=1wlap-WyQ-Qi9WYsFRliBWAdXCSPg9aljr7LupDnHsI"
     content = {
         'email': task.email,
         'code': rowkey
@@ -96,7 +146,7 @@ def main(myblob: func.InputStream):
     x = requests.post(url, json=content)
     #except Exception:
     #    msg = 'Error has occured'
-     #   logging.warn(msg)
+    #   logging.warn(msg)
 
    
 
